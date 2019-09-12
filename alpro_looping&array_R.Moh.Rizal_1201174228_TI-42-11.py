0f02e26ec8d7b9bb56c28a9913ac2c1971dd94c3
mohrizalramdani <54705054+mohrizalramdani@users.noreply.github.com>
2019-09-12 20:14:49 +0700

    Add files via upload

---
+++ alpro_looping&array_R.Moh.Rizal_1201174228_TI-42-11.py
@@ -0,0 +1,11 @@
+listGPA = [3, 2.7, 2.5, 4]
+def hadiah (GPA):
+    bonus = 500000
+    hadiah = GPA * bonus
+    return hadiah
+
+for GPA in listGPA:
+    if GPA > 3:
+        print("Total hadiah anda adalah: Rp", hadiah(GPA))
+    else:
+        print("Tidak ada bonus")
